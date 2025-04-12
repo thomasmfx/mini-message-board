a0092f0da7a6653014a5ce131027d84e88f082e7
thomasmfx <thomasmoisesf@gmail.com>
2025-04-12 11:28:17 -0300

    🐳 chore: Structure project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('node:path')
+
+const app = express();
+
+// View engine
+app.set('views', path.join('views'));
+app.set('view engine', 'ejs');
+
+// Styles
+const assetsPath = path.join('public');
+app.use(express.static(assetsPath));
+
+// App
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Application running - Listening on http://localhost:${PORT}`)
+})
