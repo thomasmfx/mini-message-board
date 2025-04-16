bd59237450b5cce1dfcf2a38142a14c32c0a60df
thomasmfx <thomasmoisesf@gmail.com>
2025-04-16 14:22:52 -0300

    🌟 feat: Add error treatment

--- src/app.js
+++ src/app.js
@@ -8,13 +8,20 @@ const app = express();
 const assetsPath = path.join('public');
 app.use(express.static(assetsPath));
 
+app.set('views', path.join('views'));
+app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/message', routes.message);
 app.use('/newMessage', routes.newMessage);
 app.use('/', routes.index);
 
-const PORT = process.env.PORT || 4000;
+app.use((err, req, res, next) => {
+  res.render('error', { error: err })
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Application running - Listening on http://localhost:${PORT}`)
 })
--- src/routes/messageRouter.js
+++ src/routes/messageRouter.js
@@ -7,11 +7,19 @@ const messageRouter = Router();
 messageRouter.set('views', path.join('views'));
 messageRouter.set('view engine', 'ejs');
 
-messageRouter.get('/:messageId', (req, res) => {
+messageRouter.get('/:messageId', (req, res, next) => {
   const { messageId } = req.params;
+  const message = db.messages[messageId];
+
+  if (!message) {
+    const error = new Error();
+    error.status = 404;
+    error.message = 'Message not found';
+    return next(error);
+  }
 
   res.render('message', {
-    message: db.messages[messageId],
+    message: message,
   })
 })
 
