4f239ed5c937df1028d7ac4e6c8525348b9817c1
thomasmfx <thomasmoisesf@gmail.com>
2025-04-22 19:02:55 -0300

    🌟 feat: Add form validation on backend

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,5 +1,20 @@
 const db = require('../db/queries')
 const utils = require('../utils/date')
+const { body, validationResult } = require('express-validator')
+
+const alphaError = 'must contain only alphanumeric characters.';
+const lengthError = (min, max) =>
+  !min
+    ? `must be less than ${max} characters.`
+    : `must be between ${min} and ${max} characters.`
+
+const validateResult = [
+  body('username').trim()
+    .isLength({ min: 1, max: 25 }).withMessage(`username ${lengthError(3, 25)}`)
+    .isAlphanumeric().withMessage(`username ${alphaError}`),
+  body('messageText').trim()
+    .isLength({ min: 1, max: 150 }).withMessage(`message ${lengthError(1, 150)}`),
+]
 
 const messagesListGet = async (req, res) => {
   const messages = await db.getAllMessages()
@@ -13,8 +28,6 @@ const messagesListGet = async (req, res) => {
 const messagesGet = async (req, res, next) => {
   const { messageId } = req.params;
   const message = await db.getMessageById(messageId)
-
-  console.log(message)
 
   if (!message) {
     const error = new Error();
@@ -32,16 +45,29 @@ const messagesNewGet = async (req, res) => {
   res.render('new')
 }
 
-const messagesNewPost = async (req, res) => {
-  const message = {
-    username: req.body.username,
-    text: req.body.messageText
+const messagesNewPost = [
+  validateResult,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render('new', {
+        errors: errors.array()
+      });
+    }
+
+    const message = {
+      username: req.body.username,
+      text: req.body.messageText
+    }
+
+    await db.insertMessage(message.username, message.text)
+
+    setTimeout(() => {
+      res.redirect('/')
+    }, 1500) // animation duration
   }
-
-  await db.insertMessage(message.username, message.text)
-
-  res.json({ success: true })
-}
+]
 
 module.exports = {
   messagesListGet,
